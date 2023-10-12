839f6ffc1d5d40b9ed65837a69ae1df7c3530728
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-10-12 16:05:26 -0300

    Criar resource de envio me mensagem

---
+++ kafka/src/main/java/com/github/andregpereira/springbootprojects/kafka/app/resource/KafkaResource.java
@@ -0,0 +1,29 @@
+package com.github.andregpereira.springbootprojects.kafka.app.resource;
+
+import com.github.andregpereira.springbootprojects.kafka.app.dto.MessageDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.function.StreamOperations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("/kafka")
+public class KafkaResource {
+
+    private final StreamOperations operations;
+
+    @PostMapping()
+    public ResponseEntity<Void> sendMessage(@RequestBody MessageDto dto) {
+        log.info("Enviando DTO");
+        operations.send("processDto-in-0", MessageBuilder.withPayload(dto).build());
+        return ResponseEntity.ok().build();
+    }
+
+}
