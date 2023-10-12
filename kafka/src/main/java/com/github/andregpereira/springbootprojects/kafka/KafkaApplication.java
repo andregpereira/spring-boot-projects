09202224028944ac33d49d6f1afdced68216ef90
André Garcia Pereira <61523970+andregpereira@users.noreply.github.com>
2023-10-12 16:04:54 -0300

    Add funções para objeto

--- kafka/src/main/java/com/github/andregpereira/springbootprojects/kafka/KafkaApplication.java
+++ kafka/src/main/java/com/github/andregpereira/springbootprojects/kafka/KafkaApplication.java
@@ -1,5 +1,6 @@
 package com.github.andregpereira.springbootprojects.kafka;
 
+import com.github.andregpereira.springbootprojects.kafka.app.dto.MessageDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Slf4j
@@ -21,14 +23,42 @@ public class KafkaApplication {
     @Bean
     public Supplier<Message<String>> supplyOut() {
         return () -> {
-            log.info("Enviando mensagem");
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            log.info("Enviando String");
             return MessageBuilder.withPayload("Teste").build();
         };
     }
 
     @Bean
-    public Consumer<Message<String>> consumeIn() {
-        return m -> log.info("Mensagem recebida {}", m.getPayload());
+    public Function<Message<String>, Message<String>> processString() {
+        return m -> {
+            String payload = m.getPayload();
+            log.info("Processando String: {}", payload);
+            return MessageBuilder.withPayload(payload.toUpperCase()).build();
+        };
+    }
+
+    @Bean
+    public Function<Message<MessageDto>, Message<MessageDto>> processDto() {
+        return m -> {
+            MessageDto payload = m.getPayload();
+            log.info("Processando DTO: {}", payload);
+            return MessageBuilder.withPayload(new MessageDto(payload.message().toUpperCase())).build();
+        };
+    }
+
+    @Bean
+    public Consumer<Message<String>> consumeString() {
+        return m -> log.info("String recebida: {}", m.getPayload());
+    }
+
+    @Bean
+    public Consumer<Message<MessageDto>> consumeDto() {
+        return m -> log.info("DTO recebida: {}", m.getPayload());
     }
 
 }
